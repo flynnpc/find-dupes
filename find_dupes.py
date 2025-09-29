3b49cffcfb19530c0a41cf15d41f6616b1202f73
flynnpc <paul.flynn98@gmail.com>
2025-09-29 06:53:30 -0700

    refactor: remove zip file handling functions and streamline hash_files logic

--- find_dupes.py
+++ find_dupes.py
@@ -4,7 +4,6 @@
 import hashlib
 import os
 import shutil
-import zipfile
 
 
 def move_file(source, destination):
@@ -29,15 +28,14 @@ def hash_files(directory):
     file_hashes = {}
 
     for filename in os.listdir(directory):
-        if not filename.endswith(".zip"):
-            file_path = os.path.join(directory, filename)
+        file_path = os.path.join(directory, filename)
 
-            if os.path.isfile(file_path):
-                with open(file_path, "rb") as file:
-                    file_content = file.read()
-                    file_hash = hashlib.md5(file_content).hexdigest()
+        if os.path.isfile(file_path):
+            with open(file_path, "rb") as file:
+                file_content = file.read()
+                file_hash = hashlib.md5(file_content).hexdigest()
 
-                    file_hashes[file_hash] = file_path
+                file_hashes[file_hash] = file_path
 
     return file_hashes
 
@@ -62,38 +60,8 @@ def find_unique_file_paths(source_hashes, directory_to_check_hashes):
     return unique_files
 
 
-def are_there_zips(directory):
-    """
-    Check if there are any zip files in the given directory.
-
-    Args:
-    directory (str): The path to the directory to check.
-
-    Returns:
-    bool: True if there are zip files, False otherwise.
-    """
-    for filename in os.listdir(directory):
-        if filename.endswith(".zip"):
-            return True
-    return False
-
-
-def extract_zip_files(directory):
-    """
-    Extract all zip files in the given directory.
-
-    Args:
-    directory (str): The path to the directory containing zip files.
-    """
-    for filename in os.listdir(directory):
-        if filename.endswith(".zip"):
-            file_path = os.path.join(directory, filename)
-            with zipfile.ZipFile(file_path, "r") as zip_ref:
-                zip_ref.extractall(directory)
-
-
 def main():
-    source_directory = input("Enter the source directory path: ")
+    target_directory = input("Enter the source directory path: ")
     directory_to_check = input("Enter the directory path to check for duplicates: ")
 
     current_directory = os.path.dirname(os.path.realpath(__file__))
@@ -102,16 +70,11 @@ def main():
     os.makedirs(os.path.join(current_directory, "unique_files"), exist_ok=True)
 
     source_hashes = []
-    if are_there_zips(source_directory):
-        extract_zip_files(source_directory)
 
-    source_hashes = hash_files(source_directory)
+    source_hashes = hash_files(target_directory)
     print(source_hashes)
 
     directory_to_check_hashes = []
-    if are_there_zips(directory_to_check):
-        extract_zip_files(directory_to_check)
-
     directory_to_check_hashes = hash_files(directory_to_check)
     print(directory_to_check_hashes)
 
