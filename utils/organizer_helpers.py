4fab5bff5ce76e93767fc5c9007dc744c1181ef2
flynnpc <paul.flynn98@gmail.com>
2025-12-25 07:27:19 -0800

    Changed from using exiftool execute_json() to get_tags() for batch processing and improved error handling.

--- utils/organizer_helpers.py
+++ utils/organizer_helpers.py
@@ -4,37 +4,36 @@ import exiftool
 
 from utils.error_handling import exiftool_read_error, metadata_read_file_is_not_file
 
-exiftool_params = [
-    "-Createate",  # Tag to search for in metadata
-    "-w",  # Date format to output
-    "%Y-%m-%d_%H:%M:%S",  # Desired date format
-]
+exiftool_tags = ["CreateDate"]
+exiftool_params = ["-d %Y-%m-%d_%H:%M:%S"]
 
 
 def walk_and_create_date_tags(directory: str) -> list[dict[str, str]]:
     with exiftool.ExifToolHelper() as et:
 
-        files_metada = []
+        files_metadata = []
 
         media_files = os.walk(directory)
+        file_paths_to_process = []
+
         for root, _, files in media_files:
             for filename in files:
                 file_path = os.path.join(root, filename)
-                if not os.path.isfile(file_path):
+
+                if os.path.isfile(file_path):
+                    file_paths_to_process.append(file_path)
+                else:
                     metadata_read_file_is_not_file(file_path)
                     continue
-                try:
-                    metadata = et.execute_json(
-                        *[
-                            f"{file_path}",
-                            *exiftool_params,
-                        ]
-                    )
-                    for tags in metadata:
-                        files_metada.append(tags)
+        try:
+            metadata = et.get_tags(
+                file_paths_to_process, exiftool_tags, exiftool_params
+            )
 
-                except Exception as e:
-                    exiftool_read_error(file_path, e)
-                    continue
+            for tags in metadata:
+                files_metadata.append(tags)
 
-        return files_metada
+        except Exception as e:
+            exiftool_read_error(file_path, e)
+
+        return files_metadata
